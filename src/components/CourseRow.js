c30ef594b76c62f08369d476e2f16168a3ee54c1
patelvp <patel.visha@husky.neu.edu>
2018-05-18 18:22:21 -0400

    Working on course Manager

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -1,9 +1,15 @@
 import React from 'react';
 class CourseRow extends React.Component {
+    constructor(props)
+    {
+        super(props);
+    }
     render() {
         return (
+            <div>
             <tr><td>Course Row</td></tr>
-        )
-    }
-}
+            <tr><td>{this.props.course.title}</td></tr>
+            </div>
+    )}}
+
 export default CourseRow;
--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import CourseRow from './CourseRow';
+import CourseRow from '../components/CourseRow';
+import CourseService from '../services/CourseService';
 
 class CourseList extends React.Component {
+    constructor() {
+        super();
+        this.courseService = CourseService.instance;
+        this.state = {courses: []};
+    }
+    componentDidMount() {
+        this.findAllCourses();
+    }
+    findAllCourses() {
+        this.courseService
+            .findAllCourses()
+            .then((courses) => {
+                console.log(courses);
+                this.setState({courses: courses});
+            })
+    }
+
+    courseRows() {
+        var rows = this.state.courses.map(function(course) {
+            return <CourseRow course={course}/>
+        });
+        return (
+            rows
+        )}
+
+
     render() {
+
         return (
             <div>
                 <h2>Course List</h2>
-                <table>
+                <table className="table">
                     <thead><tr><th>Title</th></tr></thead>
                     <tbody>
-                    <CourseRow/>
-                    <CourseRow/>
-                    <CourseRow/>
+                    {this.courseRows()}
                     </tbody>
                 </table>
             </div>
