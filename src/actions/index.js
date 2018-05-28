0148b1c98e7407d16409da2279a467ee5f98a91a
patelvp <patel.visha@husky.neu.edu>
2018-05-28 13:20:22 -0400

    Started with reducer

---
+++ src/actions/index.js
@@ -0,0 +1,85 @@
+import * as constants from "../constants/index"
+
+export const headingTextChanged = (dispatch, widgetId, newText) => (
+  dispatch({
+    type: constants.HEADING_TEXT_CHANGED,
+    id: widgetId,
+    text: newText})
+)
+export const paragraphTextChanged = (dispatch, widgetId, newText) => (
+    dispatch({
+        type: constants.PARAGRAPH_TEXT_CHANGED,
+        id: widgetId,
+        text: newText})
+)
+export const listTextChanged = (dispatch, widgetId, newText) => (
+    dispatch({
+        type: constants.LIST_TEXT_CHANGED,
+        id: widgetId,
+        text: newText})
+)
+export const listTypeChanged = (dispatch, widgetId, ListType) => (
+    dispatch({
+        type: constants.LIST_TYPE_CHANGED,
+        id: widgetId,
+        listType: ListType})
+)
+export const imageURLChanged = (dispatch, widgetId, imageURL) => (
+    dispatch({
+        type: constants.IMAGE_URL_CHANGED,
+        id: widgetId,
+        src: imageURL})
+)
+export const linkURLChanged = (dispatch, widgetId, href) => (
+    dispatch({
+        type: constants.LINK_URL_CHANGED,
+        id: widgetId,
+        href: href})
+)
+export const linkTextChanged = (dispatch, widgetId, name) => (
+    dispatch({
+        type: constants.LINK_TEXT_CHANGED,
+        id: widgetId,
+        name: name})
+)
+export const headingSizeChanged = (dispatch, widgetId, newSize) => (
+  dispatch({
+    type: constants.HEADING_SIZE_CHANGED,
+    id: widgetId,
+    size: newSize})
+)
+
+export const findAllWidgets = dispatch => {
+    console.log("in find all widgets")
+  fetch('http://localhost:8080/api/widget')
+    .then(response => (response.json()))
+    .then(widgets => dispatch({
+      type: constants.FIND_ALL_WIDGETS,
+      widgets: widgets }))
+}
+export const addWidget = dispatch => (
+  dispatch({type: constants.ADD_WIDGET})
+)
+export const save = dispatch => (
+  dispatch({type: constants.SAVE})
+)
+export const preview = dispatch => (
+  dispatch({type: constants.PREVIEW})
+)
+
+export const deleteWidget = (dispatch,widget) => (dispatch({type: constants.DELETE_WIDGET, id: widget.id})
+)
+export const increaseOrder = (dispatch,widget) =>{
+    return dispatch({type: constants.INCREASE_ORDER_WIDGET, order1: widget.order1})}
+
+export const decreaseOrder = (dispatch,widget) =>{
+    return dispatch({type: constants.DECREASE_ORDER_WIDGET, order1: widget.order1})}
+
+export const selectWidgetType = (dispatch,widget,selectElement) =>(dispatch({
+    type: constants.SELECT_WIDGET_TYPE,
+    id: widget.id,
+    className: selectElement.value
+}))
+
+export const sortWidgets = (dispatch) => (dispatch({type :constants.SORT_WIDGETS}))
+
---
+++ src/reducers/widgetReducer.js
@@ -0,0 +1,217 @@
+import * as constants from "../constants/index"
+
+export const widgetReducer = (state = {widgets: [], preview: false}, action) => {
+    let newState
+
+    function GetSortOrder(id) {
+        return function (a, b) {
+            if (a[id] > b[id]) {
+                return 1;
+            } else if (a[id] < b[id]) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    switch (action.type) {
+
+        case constants.PREVIEW:
+            return {
+                widgets: state.widgets,
+                preview: !state.preview
+            }
+
+        case constants.LIST_TYPE_CHANGED:
+            return {
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.id) {
+                        widget.listType = action.listType
+                    }
+                    return Object.assign({}, widget)
+                })
+            }
+        case constants.IMAGE_URL_CHANGED:
+            return {
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.id) {
+                        widget.src = action.src
+                    }
+                    return Object.assign({}, widget)
+                })
+            }
+        case constants.LINK_URL_CHANGED:
+            return {
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.id) {
+                        widget.href = action.href
+                    }
+                    return Object.assign({}, widget)
+                })
+            }
+        case constants.LINK_TEXT_CHANGED:
+            return {
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.id) {
+                        widget.name = action.name
+                    }
+                    return Object.assign({}, widget)
+                })
+            }
+
+        case constants.LIST_TEXT_CHANGED:
+            return {
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.id) {
+                        widget.text = action.text
+                    }
+                    return Object.assign({}, widget)
+                })
+            }
+        case constants.HEADING_TEXT_CHANGED:
+            return {
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.id) {
+                        widget.text = action.text
+                    }
+                    return Object.assign({}, widget)
+                })
+            }
+        case constants.PARAGRAPH_TEXT_CHANGED:
+            return {
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.id) {
+                        widget.text = action.text
+                    }
+                    return Object.assign({}, widget)
+                })
+            }
+
+        case constants.HEADING_SIZE_CHANGED:
+            return {
+                widgets: state.widgets.map(widget => {
+                    if (widget.id === action.id) {
+                        widget.size = action.size
+                    }
+                    return Object.assign({}, widget)
+                })
+            }
+
+        case constants.SELECT_WIDGET_TYPE:
+            console.log(action);
+            let newState = {
+                widgets: state.widgets.filter((widget) => {
+                    if (widget.id === action.id) {
+                        widget.className = action.className
+                        if (widget.className == 'List') {
+                            widget.listType = 1;
+                        }
+                        if (widget.className == 'Image') {
+                            widget.src = 'http://tinypic.com/r/2wd0ojl'
+                        }
+                        if (widget.className == 'Link') {
+                            widget.href = 'http://www.google.com'
+                            widget.name = 'Google'
+                        }
+                    }
+                    return true;
+                })
+            }
+            return JSON.parse(JSON.stringify(newState))
+
+        case constants.SAVE:
+            console.log(state.widgets)
+            fetch('http://localhost:8080/api/widget', {
+                method: 'post',
+                body: JSON.stringify(state.widgets),
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+            return state
+
+        case constants.FIND_ALL_WIDGETS:
+            newState = Object.assign({}, state)
+            if (action.widgets.status === 500) {
+                return newState
+            }
+            else {
+
+                newState.widgets = action.widgets.sort(GetSortOrder('order1'))
+
+                return newState
+            }
+
+        case constants.DELETE_WIDGET:
+            return {
+                widgets: state.widgets.filter(widget => (
+                    widget.id !== action.id
+                ))
+            }
+        case constants.INCREASE_ORDER_WIDGET:
+            console.log(state)
+            if(action.order1 === state.widgets.length){
+                return {
+                    widgets:state.widgets.map(widget => {
+                        return Object.assign({}, widget)
+                    })
+                }
+            }
+            else{
+              return {
+                  widgets:state.widgets.map(widget => {
+                      if (widget.order1 === action.order1) {
+                          widget.order1 = widget.order1+1
+                      }
+                      else if(widget.order1=== (action.order1 + 1)){
+                          widget.order1 = widget.order1-1
+                      }
+                      return Object.assign({}, widget)
+                  })
+              }}
+        case constants.DECREASE_ORDER_WIDGET:
+            if(action.order1 === 1){
+                return {
+                    widgets:state.widgets.map(widget => {
+                        return Object.assign({}, widget)
+                    })
+                }
+            }
+            else {
+                return {
+                    widgets: state.widgets.map(widget => {
+                        if (widget.order1 === action.order1) {
+                            widget.order1 = widget.order1 - 1
+                        }
+                        else if (widget.order1 === (action.order1 - 1)) {
+                            widget.order1 = widget.order1 + 1
+                        }
+                        return Object.assign({}, widget)
+                    })
+                }
+            }
+        case constants.ADD_WIDGET:
+            console.log("in add")
+            return {
+                widgets: [
+                    ...state.widgets,
+                    {
+                        id: state.widgets.length + 1,
+                        order1: state.widgets.length + 1,
+                        text: 'New Widget',
+                        className: 'Paragraph',
+                        size: '2'
+                    }
+                ]
+            }
+
+        case constants.SORT_WIDGETS:
+            console.log(state)
+            newState = Object.assign({}, state)
+            newState.widgets = newState.widgets.sort(GetSortOrder('order1'))
+            return newState
+
+        default:
+            return state
+    }
+}
