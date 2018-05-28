36cca2f161aff5f28dcb9167e948e9979debd630
patelvp <patel.visha@husky.neu.edu>
2018-05-28 13:21:37 -0400

    added Heading widget

---
+++ src/containers/HeadingContainer.js
@@ -0,0 +1,16 @@
+import React from  'react'
+import {connect} from 'react-redux'
+import Heading from "../components/Heading";
+import * as actions from "../actions";
+
+const dispathToPropsMapper = dispatch => ({
+    headingTextChanged: (widgetId, newText) =>
+        actions.headingTextChanged(dispatch, widgetId, newText),
+    headingSizeChanged: (widgetId, newSize) =>
+        actions.headingSizeChanged(dispatch, widgetId, newSize)
+})
+const stateToPropsMapper = state => ({
+    preview: state.preview
+})
+const HeadingContainer = connect(stateToPropsMapper, dispathToPropsMapper)(Heading)
+export default HeadingContainer;
